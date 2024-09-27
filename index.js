2ae7d7f6c320338498d851ce9713a9cbb271b887
florence <loracodes3@gmail.com>
2024-09-27 18:13:06 +0300

    toLowerCase string method

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
  const Sentence ="I am happy that I'm conquering my fears and i have ventured fully into Software Engineering.";
 console.log(Sentence.toUpperCase())
 
+//the output will all be capitalized: I AM HAPPY THAT I'M CONQUERING MY FEARS AND I HAVE VENTURED FULLY INTO SOFTWARE ENGINEERING.
 //you have to call the function by putting the () to call the function
+
+//toLowerCase() string method
+const Happiness="I am happy that I'm conquering my fears and i have ventured fully into Software Engineering.";
+console.log(Happiness.toLowerCase())
+// the output for the above code will be all in small letters.
