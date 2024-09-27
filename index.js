1d02289466a8ec25f36cba7d490510cd94fc1cd7
florence <loracodes3@gmail.com>
2024-09-27 18:03:12 +0300

    toUpperCase string method

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const Sentence ="I am happy that I'm conquering my fears and i have ventured fully into Software Engineering.";
+//toUpperCase() string method
+ const Sentence ="I am happy that I'm conquering my fears and i have ventured fully into Software Engineering.";
+console.log(Sentence.toUpperCase())
+
+//you have to call the function by putting the () to call the function
