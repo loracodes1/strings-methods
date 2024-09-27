07046c142cf5a16d522bae429ceb2211b16ee9ad
florence <loracodes3@gmail.com>
2024-09-27 18:34:28 +0300

    using slice()to create new strings

--- index.js
+++ index.js
@@ -24,3 +24,16 @@ console.log(str.slice(-4));
 
 console.log(str.slice(-9, -5));
 // Expected output: "lazy"
+
+//4.using .slice to create a new string
+
+const str1 = "The morning is upon us."; // The length of str1 is 23.
+const str2 = str1.slice(1, 8);
+const str3 = str1.slice(4, -2);
+const str4 = str1.slice(12);
+const str5 = str1.slice(30);
+console.log(str2); // he morn
+console.log(str3); // morning is upon u
+console.log(str4); // is upon us.
+console.log(str5); // ""
+
