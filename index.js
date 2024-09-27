240448dc19635851e2f0b3228deb753bb71f09e9
florence <loracodes3@gmail.com>
2024-09-27 18:30:40 +0300

    final commit

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ console.log(Happiness.toLowerCase())
 // the output for the above code will be all in small letters.
 
 //3. .slice() string method
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str.slice(31));
+// Expected output: "the lazy dog."
+
+console.log(str.slice(4, 19));
+// Expected output: "quick brown fox"
+
+console.log(str.slice(-4));
+// Expected output: "dog."
+
+console.log(str.slice(-9, -5));
+// Expected output: "lazy"
