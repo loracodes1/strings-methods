09b48cab2b099a5676f158d9bf6b74fde7285185
florence <loracodes3@gmail.com>
2024-09-27 18:23:12 +0300

    slice string method

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-//toUpperCase() string method
+//1. toUpperCase() string method
  const Sentence ="I am happy that I'm conquering my fears and i have ventured fully into Software Engineering.";
 console.log(Sentence.toUpperCase())
 
 //the output will all be capitalized: I AM HAPPY THAT I'M CONQUERING MY FEARS AND I HAVE VENTURED FULLY INTO SOFTWARE ENGINEERING.
 //you have to call the function by putting the () to call the function
 
-//toLowerCase() string method
+//2.toLowerCase() string method
 const Happiness="I am happy that I'm conquering my fears and i have ventured fully into Software Engineering.";
 console.log(Happiness.toLowerCase())
 // the output for the above code will be all in small letters.
+
+//3. .slice() string method
